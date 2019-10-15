00aa8d732110db3a26e039e3e25116719af163a7
ChristopherHubbard <cbhubbard8@gmail.com>
2019-10-14 21:44:23 -0500

    Added BestFitLine model and calc for the simple regression. Now just need to hook up to the router and simple prediction should be "functional"

--- Prediction-API/Calculators/SimpleLinearRegressionCalculator.cs
+++ Prediction-API/Calculators/SimpleLinearRegressionCalculator.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Prediction_API.Models;
 
 namespace Prediction_API.Calculators
 {
     public class SimpleLinearRegressionCalculator : ICalculator
     {
-        public T Calculate<T>()
+        // Calculate the simple linear regression of a set of numbers -- what to return for this calc??
+        public BestFitLine Calculate(List<decimal> nums)
         {
-
+            // Get the best fit line -- send the list of x and y values -- this can be used with the regression to calculate predictions at different intervals!!
+            return new BestFitLine(Enumerable.Range(0, nums.Count).ToList().ConvertAll(x => (decimal) x), nums);
         }
     }
 }
